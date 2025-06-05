4818ca0389dacd3f14e2150435509d8219015747
Kanika Singhal <kanikasin216@gmail.com>
2025-06-05 19:29:46 +0530

    debug

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:8080";
+const URL = `${import.meta.env.VITE_BACKEND_URL}`;
 const socket = io(URL, {
   transports: ["websocket"],
   autoConnect: true,
