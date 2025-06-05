03dc82ab41bd50ec38bf075d4ccb91b6323630a0
Kanika Singhal <kanikasin216@gmail.com>
2025-06-05 19:05:23 +0530

    added docker links

--- server/index.js
+++ server/index.js
@@ -21,6 +21,8 @@ const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const app = express();
 connectDB();
 
+const dockerurl=process.env.COMPILER_SERVER_URL
+
 app.use(cors());
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -161,7 +163,7 @@ app.post("/execute", async (req, res) => {
   const { language, code, input } = req.body;
 
   try {
-    const response = await axios.post("http://localhost:5001/run", {
+    const response = await axios.post(`${dockerurl}/run`, {
       language,
       code,
       input,
