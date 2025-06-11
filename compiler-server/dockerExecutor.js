a53210ace05eac537dedc57b21415d66e18c1350
Kanika Singhal <kanikasin216@gmail.com>
2025-06-11 15:43:33 +0530

    final

--- compiler-server/dockerExecutor.js
+++ compiler-server/dockerExecutor.js
@@ -1,53 +1,3 @@
-// const fs = require("fs");
-// const path = require("path");
-// const { exec } = require("child_process");
-// const { v4: uuid } = require("uuid");
-
-// const executeCode = (language, code, input, callback) => {
-//   const id = uuid();
-//   const tempDir = path.join(__dirname, "temp", id);
-//   fs.mkdirSync(tempDir, { recursive: true });
-
-//   const codeFile = path.join(tempDir, `code.${language}`);
-//   const inputFile = path.join(tempDir, "input.txt");
-//   const outputFile = path.join(tempDir, "output.txt");
-
-//   fs.writeFileSync(codeFile, code);
-//   fs.writeFileSync(inputFile, input);
-
-//   const volumeMount = tempDir.replace(/\\/g, "/");
-
-//   let dockerCommand = "";
-//   if (language === "cpp") {
-//     dockerCommand = `docker run --rm -v ${volumeMount}:/app cpp-code-executor bash -c "g++ /app/code.cpp -o /app/a.out && /app/a.out < /app/input.txt | tee /app/output.txt"`;
-//   } else if (language === "python") {
-//     dockerCommand = `docker run --rm -v ${volumeMount}:/app python:3.10 bash -c "python3 /app/code.python < /app/input.txt | tee /app/output.txt"`;
-//   } else if (language === "java") {
-//     dockerCommand = `docker run --rm -v ${volumeMount}:/app openjdk:17 bash -c "javac /app/code.java && java -cp /app Main < /app/input.txt | tee /app/output.txt"`;
-//   } else if (language === "javascript") {
-//     dockerCommand = `docker run --rm -v ${volumeMount}:/app node:20 bash -c "node /app/code.javascript < /app/input.txt | tee /app/output.txt"`;
-//   }
-
-//   exec(dockerCommand, (error, stdout, stderr) => {
-//     if (error || stderr) {
-//       return callback(stderr || error.message);
-//     }
-
-//     try {
-//       const output = fs.readFileSync(outputFile, "utf8");
-//       callback(output);
-//     } catch (err) {
-//       callback("Error reading output file.");
-//     }
-
-    
-//     fs.rmSync(tempDir, { recursive: true, force: true });
-//   });
-// };
-
-// module.exports = executeCode;
-
-
 const fs = require("fs");
 const path = require("path");
 const { exec } = require("child_process");
@@ -72,7 +22,6 @@ const executeCode = (language, code, input, callback) => {
   fs.writeFileSync(codeFile, code);
   fs.writeFileSync(inputFile, input);
 
-  // Commands per language
   let command = "";
 
   if (language === "cpp") {
@@ -83,23 +32,31 @@ const executeCode = (language, code, input, callback) => {
     command = `javac ${codeFile} && java -cp ${tempDir} Main < ${inputFile} > ${outputFile}`;
   } else if (language === "javascript") {
     command = `node ${codeFile} < ${inputFile} > ${outputFile}`;
+  } else {
+    return callback(null, "Unsupported language");
   }
 
   exec(command, { cwd: tempDir }, (error, stdout, stderr) => {
+    let output = "";
+
     if (error || stderr) {
-      return callback(stderr || error.message);
+      callback(null, stderr || error.message);
+    } else {
+      try {
+        output = fs.readFileSync(outputFile, "utf8");
+        callback(output, null); // ✅ Corrected: success
+      } catch (readErr) {
+        callback(null, "Error reading output file.");
+      }
     }
 
+    // Clean up
     try {
-      const output = fs.readFileSync(outputFile, "utf8");
-      callback(output);
-    } catch (err) {
-      callback("Error reading output file.");
+      fs.rmSync(tempDir, { recursive: true, force: true });
+    } catch (cleanupErr) {
+      console.error("Cleanup failed:", cleanupErr);
     }
-
-    fs.rmSync(tempDir, { recursive: true, force: true });
   });
 };
 
 module.exports = executeCode;
-
